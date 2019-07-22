04f970c2d095332e28e82168cda63e99fbb0523e
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-22 16:50:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
   answer = ""
-  if ((input / 3).round == input / 3)
+  if ((input.to_f / 3).round == input / 3)
     answer = "Fizz"
   elsif ((input / 5).round == input / 5)
     answer = "#{answer}Buzz"
