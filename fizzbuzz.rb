66b7831cd7d86179853d20c0566733a35418d56f
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-22 16:36:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
   if (input.round / 3 == input / 3)
+    return "Fizz"
+  else if (input.round / 5 == input / 5)
+    return "Buzz"
+  else if (input.round / 15 == input / 15)
+    return "Fizzbuzz"
 end
