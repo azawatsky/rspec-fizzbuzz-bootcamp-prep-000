52f6d2d52081af7acf1d3c3b8343cf1c08cad548
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-22 16:33:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
-  if (input.to_i / 3 == input / 3)
+  if (input.round / 3 == input / 3)
 end
