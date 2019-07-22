ba359132f3e537e77480984cdd0619429178f68b
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-22 16:39:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
+  answer = ""
   if (input.round / 3 == input / 3)
-    return "Fizz"
+    answer = "Fizz"
   else if (input.round / 5 == input / 5)
-    return "Buzz"
-  else if (input.round / 15 == input / 15)
-    return "Fizzbuzz"
+    answer = "#{answer}Buzz"
+  else
+    answer = nil
 end
